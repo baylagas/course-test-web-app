bae2a129fa0a5e0f9ec4765b38f62c3f32ababa7
balbino aylagas <balbino_a@hotmail.com>
2020-07-09 19:11:01 -0600

    added html:course, base, modify index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/course", methods=["GET", "POST"])
+def course():
+    if request.method == "GET":
+        print("get...")
+        return render_template("course.html")
+    else:
+        print("post...")
 
 
 if __name__ == "__main__":
